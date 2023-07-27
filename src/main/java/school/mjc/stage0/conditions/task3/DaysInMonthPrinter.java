aafd9f991387dce90d80b16652acfc8ce612b978
RamazanMamyrbek <rama.mamirbek@gmail.com>
2023-07-27 16:17:35 +0600

    s

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
+        } else {
+            int days;
+            if (month == 2) {
+                days = 28;
+            } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                days = 30;
+            } else {
+                days = 31;
+            }
+            System.out.println(days);
+        }
     }
 }
